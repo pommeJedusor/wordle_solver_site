9abccf1b7aab7ba1dab7f8863da03fb4b8319100
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-07-17 12:47:25 +0200

    placed edit button

--- components/wordle/Edit.tsx
+++ components/wordle/Edit.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export function Edit(){
   return (
-    <button type="button" title="edit word" className="cursor-pointer mx-2 p-1 rounded-full bg-slate-300 dark:bg-slate-400 hover:bg-slate-400 hover:dark:bg-slate-500 hover:ring-4 focus:ring-4 outline-none ring-slate-200 dark:ring-slate-300">
-      <Image className="w-12 h-12 mx-auto" width={500} height={500} src="/edit.svg" alt="edit word" />
+    <button type="button" title="edit word" className="cursor-pointer place-self-center row-span-2 col-span-2 w-1/2 h-1/2 mx-2 p-1 rounded-full bg-slate-300 dark:bg-slate-400 hover:bg-slate-400 hover:dark:bg-slate-500 hover:ring-4 focus:ring-4 outline-none ring-slate-200 dark:ring-slate-300">
+      <Image className="w-full h-full mx-auto" width={500} height={500} src="/edit.svg" alt="edit word"/>
     </button>
   )
 }
--- components/wordle/Grid.tsx
+++ components/wordle/Grid.tsx
@@ -4,21 +4,39 @@ import { Square } from "@/components/wordle/Square";
 import { Arrow } from "./Arrow";
 import { get_next_color, get_previous_color } from "@/utils/get_next_color";
 import { WordleGame } from "@/utils/Game";
+import { Edit } from "./Edit";
 
 function create_squares(wordle_game: WordleGame[]){
   const wordleGame = wordle_game[0]
   const squares = [];
   for(let i=0;i<wordleGame.colors.length;i++){
+    // up arrows
     for(let j=0;i==wordleGame.current_row && j<wordleGame.colors[i].length;j++){
       const previous_color = get_previous_color(wordleGame.colors[i][j]);
       squares.push(<Arrow wordleGame={wordleGame} row={i} col={j} color={previous_color} must_rotate={false} key={1000 + i * wordleGame.colors[0].length + j} />);
     }
+    if (i==wordleGame.current_row){
+      squares.push(<div className="row-span-1 col-span-2" key={5000 + i}></div>);
+    }
+
+    // word
     for(let j=0;j<wordleGame.colors[i].length;j++){
       squares.push(<Square wordleGame={wordleGame} row={i} letter={wordleGame.letters[i][j]} color={wordleGame.colors[i][j]} key={i * wordleGame.colors[0].length + j}/>);
     }
+    // edit button
+    if (i==wordleGame.current_row){
+      squares.push(<Edit key={3000 + i}/>);
+    }else{
+      squares.push(<div className="row-span-2 col-span-2" key={5000 + i}></div>);
+    }
+
+    // down arrows
     for(let j=0;i==wordleGame.current_row && j<wordleGame.colors[i].length;j++){
       const next_color = get_next_color(wordleGame.colors[i][j]);
       squares.push(<Arrow wordleGame={wordleGame} row={i} col={j} color={next_color} must_rotate={true} key={2000 + i * wordleGame.colors[0].length + j} />);
+    }
+    if (i==wordleGame.current_row){
+      squares.push(<div className="row-span-1 col-span-2" key={4000 + i}></div>);
     }
   }
   return squares;
@@ -26,7 +44,7 @@ function create_squares(wordle_game: WordleGame[]){
 
 export function Grid({wordleGame}: {wordleGame: WordleGame[]}) {
   return (
-    <div className="uppercase grid grid-rows-14 grid-cols-10 gap-2 h-141 w-100 dark:bg-background-night bg-background-day">
+    <div className="uppercase grid grid-rows-14 grid-cols-12 gap-2 h-180 w-150 dark:bg-background-night bg-background-day">
       {create_squares(wordleGame)}
     </div>
   );
