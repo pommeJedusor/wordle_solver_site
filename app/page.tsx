771f2be781078ac986ad54038d3935ef99c3018b
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-07-22 14:13:02 +0200

    add letter keys to keyboard

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           </button>
         </div>
         <Grid wordleGame={wordleGame}/>
-        <KeyBoard/>
+        <KeyBoard wordleGame={wordleGame}/>
     </div>
   );
 }
--- components/wordle/KeyBoard.tsx
+++ components/wordle/KeyBoard.tsx
@@ -1,9 +1,84 @@
 "use client";
 
-export function KeyBoard(){
+import { WordleGame } from "@/utils/Game";
+
+enum Color {
+  Default,
+  NotInWord,
+  WellPlaced,
+  WronglyPlaced,
+}
+
+
+function DefaultLetterKey({letter}: {letter: string}){
   return (
-    //border-2 border-square-border-day
-    <div className="w-90 sm:w-122 md:w-140 h-60 sm:h-50 md:h-60 border-2 border-black-2">
+    <div className="grid h-full basis-2/23 bg-square-border-day dark:bg-square-border-night text-black dark:text-white rounded-md cursor-pointer">
+      <p className="select-none font-[1000] place-self-center">{letter}</p>
     </div>
   )
+}
+
+function WellPlacedLetterKey({letter}: {letter: string}){
+  return (
+    <div className="grid h-full basis-2/23 bg-well-placed-letter-day dark:bg-well-placed-letter-night text-white rounded-md cursor-pointer">
+      <p className="select-none font-[1000] place-self-center">{letter}</p>
+    </div>
+  )
+}
+
+function WronglyPlacedLetterKey({letter}: {letter: string}){
+  return (
+    <div className="grid h-full basis-2/23 bg-valid-letter-day dark:bg-valid-letter-night text-white rounded-md cursor-pointer">
+      <p className="select-none font-[1000] place-self-center">{letter}</p>
+    </div>
+  )
+}
+
+function NotInWordLetterKey({letter}: {letter: string}){
+  return (
+    <div className="grid h-full basis-2/23 bg-unvalid-letter-day dark:bg-unvalid-letter-night text-white rounded-md cursor-pointer">
+      <p className="select-none font-[1000] place-self-center">{letter}</p>
+    </div>
+  )
+}
+
+export function KeyBoard({wordleGame}: {wordleGame: WordleGame[]}){
+  function generateLetterKeys(letters: string){
+    const result = [];
+    for (const letter of letters){
+      const letter_color = getColorOfLetter(letter, wordleGame[0]);
+      if (letter_color == Color.WellPlaced)result.push(<WellPlacedLetterKey letter={letter} key={letter}/>);
+      else if (letter_color == Color.WronglyPlaced)result.push(<WronglyPlacedLetterKey letter={letter} key={letter}/>);
+      else if (letter_color == Color.NotInWord)result.push(<NotInWordLetterKey letter={letter} key={letter}/>);
+      else result.push(<DefaultLetterKey letter={letter} key={letter}/>);
+    }
+    return result;
+  }
+
+  return (
+    <div className="text-1xl sm:text-1xl md:text-2xl grid grid-flow-col grid-rows-3 gap-2 uppercase w-90 sm:w-122 md:w-140 h-60 sm:h-50 md:h-60">
+      <div className="flex justify-center gap-2">
+        {generateLetterKeys("qwertyuiop")}
+      </div>
+      <div className="flex justify-center gap-2">
+        {generateLetterKeys("asdfghjkl")}
+      </div>
+      <div className="flex justify-center gap-2">
+        {generateLetterKeys("zxcvbnm")}
+      </div>
+    </div>
+  )
+}
+
+function getColorOfLetter(letter: string, wordleGame: WordleGame): Color{
+  let color = Color.Default;
+  for (let i=0;i<wordleGame.letters.length;i++){
+    for (let j=0;j<wordleGame.letters[0].length;j++){
+      if (wordleGame.letters[i][j] != letter)continue;
+      if (wordleGame.colors[i][j] == "G")return Color.WellPlaced;
+      else if (wordleGame.colors[i][j] == "Y")color = Color.WronglyPlaced;
+      else if (wordleGame.colors[i][j] == "B" && color != Color.WronglyPlaced)color = Color.NotInWord;
+    }
+  }
+  return color;
 }
