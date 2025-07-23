7d8a57fa7897b427fd954f1c3cf06363bfdf1b0e
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-07-23 13:52:26 +0200

    increased edit button size

--- components/wordle/Edit.tsx
+++ components/wordle/Edit.tsx
@@ -16,7 +16,7 @@ export function Edit({wordleGame}: {wordleGame: WordleGame}){
   }
 
   return (
-    <button ref={buttonRef} onClick={toggleEditMode} type="button" title="edit word" className="cursor-pointer place-self-center row-span-2 col-span-2 w-1/2 h-1/2 mx-2 p-1 rounded-full bg-slate-300 dark:bg-slate-400 hover:bg-slate-400 hover:dark:bg-slate-500 hover:ring-4 focus:ring-4 outline-none ring-slate-200 dark:ring-slate-300">
+    <button ref={buttonRef} onClick={toggleEditMode} type="button" title="edit word" className="cursor-pointer place-self-center row-span-2 col-span-2 w-5/8 h-5/8 mx-2 p-1 rounded-full bg-slate-300 dark:bg-slate-400 hover:bg-slate-400 hover:dark:bg-slate-500 hover:ring-4 focus:ring-4 outline-none ring-slate-200 dark:ring-slate-300">
       <Image className="w-full h-full mx-auto" width={500} height={500} src="/edit.svg" alt="edit word"/>
     </button>
   )
