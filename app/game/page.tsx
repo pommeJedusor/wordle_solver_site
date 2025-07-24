2c6fc91575b99a06681318658f03e918ff4de8c5
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-07-24 14:20:44 +0200

    unfocus buttons once pressed

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -3,10 +3,16 @@ import { Grid } from "@/components/wordle/Grid";
 import { KeyBoard } from "@/components/wordle/KeyBoard";
 import { GameState, WordleGame } from "@/utils/Game";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 export default function Home() {
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+  function unfocusResetGameButton(){
+    buttonRef.current?.blur();
+  }
+
   useEffect(()=>{
     const func = (event: KeyboardEvent) => {wordleGame[0].keyPressEventListener(event.key)};
     addEventListener("keydown", func);
@@ -23,7 +29,7 @@ export default function Home() {
   return (
       <div is-game="true" className="flex flex-col items-center gap-10 dark:bg-background-night bg-background-day min-h-dvh">
         <div className="flex flex-row justify-center mt-10">
-          <button onClick={resetGame} type="button" title="refresh" className="cursor-pointer mx-2 focus:outline-none hover:outline-none focus:ring-4 hover:ring-4 focus:ring-sky-700 dark:focus:ring-sky-800 hover:ring-sky-600 dark:hover:ring-sky-700 bg-sky-500 dark:bg-sky-600 hover:bg-sky-400 dark:hover:bg-sky-500 p-1 rounded" >
+          <button ref={buttonRef} onClick={()=>{resetGame();unfocusResetGameButton()}} type="button" title="refresh" className="cursor-pointer mx-2 focus:outline-none hover:outline-none focus:ring-4 hover:ring-4 focus:ring-sky-700 dark:focus:ring-sky-800 hover:ring-sky-600 dark:hover:ring-sky-700 bg-sky-500 dark:bg-sky-600 hover:bg-sky-400 dark:hover:bg-sky-500 p-1 rounded" >
             <Image className="md:w-12 sm:w-9 w-6 md:h-12 sm:h-9 h-6 mx-auto rotate-270" width={500} height={500} src="/refresh.svg" alt="refresh" />
           </button>
         </div>
