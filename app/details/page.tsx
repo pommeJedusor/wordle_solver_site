1de5bfb6ee69a36b617ad8f3135ba51f8e81b400
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-07-31 10:46:42 +0200

    made details page more responsive

--- app/details/page.tsx
+++ app/details/page.tsx
@@ -3,12 +3,13 @@ import NavBar from "@/components/wordle/NavBar";
 
 export default function Home() {
   return (
+    //md:w-12 sm:w-9 w-6
     <div is-game="true" className="flex flex-col items-center gap-10 dark:bg-background-night bg-background-day min-h-dvh">
       <NavBar/>
       <h1 className="text-2xl font-[600]">Informations about the site and solver</h1>
       <section className="w-full flex flex-col items-center">
-        <h2 className="w-1/3 text-xl font-[600]">Two lists of words</h2>
-        <p className="w-1/3 text-lg mb-2">
+        <h2 className="w-full sm:w-1/2 md:w-1/3 text-xl font-[600]">Two lists of words</h2>
+        <p className="w-full sm:w-1/2 md:w-1/3 text-lg mb-2">
           the solver use two lists of words, one is all the words that are usable as guesses
           and the other one is all words that could end up being the solution
           I found those on internet, don&apos;t remember where though,
@@ -19,15 +20,15 @@ export default function Home() {
             MIT&apos;s algorithm
             </a>
         </p>
-        <p className="w-1/3 text-lg mb-2">
+        <p className="w-full sm:w-1/2 md:w-1/3 text-lg mb-2">
           from time to time the solution isn&apos;t in the list of possible solutions, so I check server side if it&apos;s the case
           and if it is I add it to the list of possible solutions
           (due to the different timezones it actually look for the previous and following day of the current one too (UTC))
         </p>
       </section>
       <section className="w-full flex flex-col items-center">
-        <h2 className="w-1/3 text-xl font-[600]">Wordle Solver, the way it works</h2>
-        <p className="w-1/3 text-lg mb-2">
+        <h2 className="w-full sm:w-1/2 md:w-1/3 text-xl font-[600]">Wordle Solver, the way it works</h2>
+        <p className="w-full sm:w-1/2 md:w-1/3 text-lg mb-2">
           the way that the algorithm works at its core is that it takes every possible guesses and for each one
           it looks at every possible solution and split them according to their color pattern<br/>
           (if the possible guess is &quot;apple&quot; then &quot;might&quot; and &quot;right&quot; will both be put in the colors BBBBB where as
@@ -40,17 +41,17 @@ export default function Home() {
         </p>
       </section>
       <section className="w-full flex flex-col items-center">
-        <h2 className="w-1/3 text-xl font-[600]">Optimisations</h2>
-        <h3 className="w-1/3 text-xl font-[500] my-2">Color patterns caching</h3>
-        <p className="w-1/3 text-lg mb-2">
+        <h2 className="w-full sm:w-1/2 md:w-1/3 text-xl font-[600]">Optimisations</h2>
+        <h3 className="w-full sm:w-1/2 md:w-1/3 text-xl font-[500] my-2">Color patterns caching</h3>
+        <p className="w-full sm:w-1/2 md:w-1/3 text-lg mb-2">
           the optimisation I first tried was to cache the color pattern for every possible possible guess-solution duo
           which worked great reducing from 90 seconds to only 12!
           though a &apos;little&apos; problem was that it takes quite a bit of memory and basically just made my vps unusable
           a bit after I deployed it <br/>
           so I sadly had to remove that optimisation
         </p>
-        <h3 className="w-1/3 text-xl font-[500] my-2">Compexity Reducing</h3>
-        <p className="w-1/3 text-lg mb-2">
+        <h3 className="w-full sm:w-1/2 md:w-1/3 text-xl font-[500] my-2">Compexity Reducing</h3>
+        <p className="w-full sm:w-1/2 md:w-1/3 text-lg mb-2">
           the second optimisation I came up with, I was doubtfull that it would work great but it really did more so that I could have thought<br/>
           the idea is actually pretty simple, instead of evaluating each guess one by one on each solution, let&apos;s just do a shallow
           evaluation before, what it means is that we&apos;ll first evaluate all possible guesses on max 100 possible solutions
@@ -62,13 +63,13 @@ export default function Home() {
             - min(n, 100) * m + n * min(m, 40) (1_577_860 max)<br/>
           which reduces the time at a bit more that a second! with that I had the possibility to implement the edit mode
         </p>
-        <h3 className="w-1/3 text-xl font-[500] my-2">Cython</h3>
-        <p className="w-1/3 text-lg mb-2">
+        <h3 className="w-full sm:w-1/2 md:w-1/3 text-xl font-[500] my-2">Cython</h3>
+        <p className="w-full sm:w-1/2 md:w-1/3 text-lg mb-2">
           the third optimisation the most simple of all, I just compiled the python code that do all the calcul to cython
           with that I achieved to go slightly under a second (on my computer at least)
         </p>
-        <h3 className="w-1/3 text-xl font-[500] my-2">Caching the first two guesses</h3>
-        <p className="w-1/3 text-lg mb-2">
+        <h3 className="w-full sm:w-1/2 md:w-1/3 text-xl font-[500] my-2">Caching the first two guesses</h3>
+        <p className="w-full sm:w-1/2 md:w-1/3 text-lg mb-2">
           for the 4th one back to the caching strategy but instead of caching the color patterns, I cached the first two guesses
           but there is a bit more to that, in that I didn&apos;t just calcul what my algorithm would give for the first guess and
           then the second based on the color pattern instead I did an extensive calcul to get yet better words in the same time<br/>
@@ -83,8 +84,8 @@ export default function Home() {
         </p>
       </section>
       <section className="w-full flex flex-col items-center">
-        <h2 className="w-1/3 text-xl font-[600]">Site</h2>
-        <p className="w-1/3 text-lg mb-2">
+        <h2 className="w-full sm:w-1/2 md:w-1/3 text-xl font-[600]">Site</h2>
+        <p className="w-full sm:w-1/2 md:w-1/3 text-lg mb-2">
           for the <a
             className="underline text-valid-letter-day hover:text-well-placed-letter-day visited:text-unvalid-letter-day dark:text-valid-letter-night dark:hover:text-well-placed-letter-night dark:visited:text-unvalid-letter-night"
             href="https://github.com/pommeJedusor/wordle_solver_site">
@@ -96,7 +97,7 @@ export default function Home() {
             backend
             </a> I used flask (python)
         </p>
-        <p className="w-1/3 text-lg mb-2">
+        <p className="w-full sm:w-1/2 md:w-1/3 text-lg mb-2">
           for the yellow/star button on the wordle-solver page, it colors all the guesses to the current included
           according to the current wordle solution, the current local day is collected using js and passing to a request to my backend,
           so that whatever the local timezone, it will color the guesses accordingly
